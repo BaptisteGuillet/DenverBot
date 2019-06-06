56796d31ef35cae6f272b213f16f3b19df87da8f
Androz2091 <androz2091@gmail.com>
2019-06-06 18:47:42 +0200

    Modif des params des commandes

--- commands/General/ping.js
+++ commands/General/ping.js
@@ -4,28 +4,27 @@ Discord = require("discord.js");
 class Ping extends Command {
     constructor (client) {
         super(client, {
-            name = "ping",
-            description = languages.get("PING_DESCRIPTION"),
-            usage = languages.get("PING_USAGE"),
-            examples = languages.get("PING_EXAMPLES"),
-            dirname = __dirname,
-            enabled = true,
-            guildOnly = false,
-            aliases = [
+            name: "ping",
+            description: languages.get("PING_DESCRIPTION"),
+            usage: languages.get("PING_USAGE"),
+            examples: languages.get("PING_EXAMPLES"),
+            dirname: __dirname,
+            enabled: true,
+            guildOnly: false,
+            aliases: [
                 "pong"
             ],
-            memberPermissions = [],
-            botPermissions = [],
-            nsfw = false,
-            adminOnly = false,
-            cooldown = 1000
+            memberPermissions: [],
+            botPermissions: [],
+            nsfw: false,
+            adminOnly: false,
+            cooldown: 1000
         });
     }
 
     async run (message, args, data) {
         message.channel.send(message.language.get("PING_PONG"));
     }
-
 }
 
 module.exports = Ping;
