8986069a7b9f476224c3adb71dfcdf56eed4c887
Androz2091 <androz2091@gmail.com>
2019-06-08 15:53:01 +0200

    Ajout de la fonction getPrefix()

--- utils/functions.js
+++ utils/functions.js
@@ -4,10 +4,8 @@ module.exports = {
 
     /**
      * Gets channel settings
-     * 
      * @param {object} client The discord client
      * @param {object} channel The channel object
-     * 
      * @returns The channel data
      */
     async getSettings(client, channel){
@@ -30,7 +28,30 @@ module.exports = {
                     language: client.config.defaultLanguage
                 });
             }
-            
         });
+    },
+
+    /**
+     * Gets message prefix
+     * @param {object} message The Discord message
+     * @returns The prefix
+     */
+    getPrefix(message){
+        if(message.settings.prefix.length > 0){
+            const prefixes = [
+                `<@${message.client.user.id}>`,
+                "Denver",
+                message.settings.prefix
+            ];
+            let prefix = null;
+            prefixes.forEach((p) => {
+                if(message.content.startsWith(p)){
+                    prefix = p;
+                }
+            });
+            return prefix;
+        } else {
+            return "";
+        }
     }
 };
