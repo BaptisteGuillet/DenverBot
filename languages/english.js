b14cb9fb89e1b1de64c85c1024462f4b97ab632a
Androz2091 <androz2091@gmail.com>
2019-06-08 15:51:09 +0200

    Ajout de la langue english

---
+++ languages/english.js
@@ -0,0 +1,38 @@
+const e = require("../config.js").emotes;
+
+module.exports = class {
+    constructor(...args) {
+		this.language = {
+
+			PREFIX_INFO: (prefix) => `${prefix.length > 0 ? `${e.info} | My prefix on this server is \`${prefix}\` !` : `${e.info} | No prefix. Simply type the command without prefix !`}`,
+
+            NO_DESCRIPTION_PROVIDED: "No description defined",
+            NO_USAGE_PROVIDED: "No defined use",
+            NO_EXAMPLES_PROVIDED: "No example defined",
+
+			ERROR_PERMISSIONS_TITLE: `${e.error} Insufficient permissions`,
+			ERROR_PERMISSIONS_CONTENT: (lvl, rlvl) => `This permission requires the permission level \`${rlvl}\` and you only have the level \`${lvl}\` !`,
+
+			ERROR_COMMAND_GUILDONLY: `${e.error} | This command is unavailable in private messages!`,
+
+            PING_DESCRIPTION: "Just answer pong",
+            PING_USAGE: (prefix) => `${prefix}ping`,
+            PING_EXAMPLES: (prefix) => `${prefix}ping`,
+            PING_PONG: "Pong !"
+
+        }
+    }
+    /**
+	 * The method to get language strings
+	 * @param {string} term The string or function to look up
+	 * @param {...*} args Any arguments to pass to the lookup
+	 * @returns {string|Function}
+	 */
+	get(term, ...args) {
+		const value = this.language[term];
+		switch (typeof value) {
+			case "function": return value(...args);
+			default: return value;
+		}
+	}
+}
--- languages/french.js
+++ languages/french.js
@@ -1,10 +1,19 @@
+const e = require("../config.js").emotes;
+
 module.exports = class {
     constructor(...args) {
 		this.language = {
 
+			PREFIX_INFO: (prefix) => `${prefix.length > 0 ? `${e.info} | Mon préfixe sur ce serveur est ${prefix} !` : `${e.info} | Aucun préfixe. Tapez simplement la commande sans préfixe !`}`,
+
             NO_DESCRIPTION_PROVIDED: "Aucune description définie",
             NO_USAGE_PROVIDED: "Aucun usage défini",
             NO_EXAMPLES_PROVIDED: "Aucun exemple défini",
+
+			ERROR_PERMISSIONS_TITLE: `${e.error} Permissions insuffisantes`,
+			ERROR_PERMISSIONS_CONTENT: (lvl, rlvl) => `Cette permission nécessite le niveau de permission \`${rlvl}\` et vous n'avez que le niveau \`${lvl}\` !`,
+
+			ERROR_COMMAND_GUILDONLY: `${e.error} | Cette commande est indisponible en messages privés !`,
 
             PING_DESCRIPTION: "Répond juste pong",
             PING_USAGE: (prefix) => `${prefix}ping`,
